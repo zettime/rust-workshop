6dc0a6e8214926b149a8ccfba9fd53754062f65c
Maximilian Kürschner <47224024+programonaut@users.noreply.github.com>
2022-02-14 18:27:50 +0100

    update: todo

--- Uebung_References_Borrowing/tic_tac_toe_lib/src/main.rs
+++ Uebung_References_Borrowing/tic_tac_toe_lib/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     // field::display_field(...);
 
     // TODO: implement do_move in src/game/field.rs
-    // input: x = 1, y = 1, player = 1
+    // x = 1, y = 1, player = 1
     // field::do_move(...);
 
     // TODO: implement display_field in src/game/field.rs
